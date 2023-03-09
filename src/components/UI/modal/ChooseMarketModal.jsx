f3e31d0414e108c27d050b05fe1a7ab2be52f63c
whackamole7 <whackamole7@proton.me>
2023-03-09 17:21:48 +0300

    ChooseMarketModal fix

--- src/components/UI/modal/ChooseMarketModal.jsx
+++ src/components/UI/modal/ChooseMarketModal.jsx
@@ -24,7 +24,7 @@ const ChooseMarketModal = (props) => {
 				if (draftMarkets.length) {
 					setMarkets(draftMarkets);
 				} else {
-					setMarkets(marketsList);
+					setMarkets(marketsList.slice());
 				}
 			}}
 		>
@@ -88,7 +88,7 @@ const ChooseMarketModal = (props) => {
 					if (draftMarkets.length) {
 						setMarkets(draftMarkets);
 					} else {
-						setMarkets(marketsList);
+						setMarkets(marketsList.slice());
 					}
 					setVisible(false);
 				}}
